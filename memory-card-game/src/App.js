bd117d0d6c19b60f3828cfeadcd530138862f820
Aodj05 <aodj05@msn.com>
2022-06-23 14:03:43 -0400

    shuffle cards

--- memory-card-game/src/App.js
+++ memory-card-game/src/App.js
@@ -36,5 +36,22 @@ const uniqueElentsArray = [
   }
 ];
 
+//shuffle logic
+function swap(array, i, j) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function shuffleCards(array) {
+  const length = array.length;
+  for (let i = length; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
+    const currentIndex = i - 1;
+    swap(array, currIndex, randomIndex)
+  }
+  return array;
+}
+
 
 export default App;
